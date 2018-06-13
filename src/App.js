4f35c9f760cfe334bdaa4714ad0c41fd9bed05e0
David Kenneth Coleman <david_coleman_007@hotmail.com>
2018-06-12 23:09:32 -0700

    attribution

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to my Newsapi.org app</h1>
         </header>
         <main>
           <Route exact path="/" component={SourceView}/>
